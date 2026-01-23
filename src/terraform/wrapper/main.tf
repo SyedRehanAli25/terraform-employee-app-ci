675709c7042374748d026cb43b30613284e40e5a
Syed Rehan Ali <rehan.ali9325@gmail.com>
2026-01-23 07:22:39 +0530

    updated main and variables.tf

--- src/terraform/wrapper/main.tf
+++ src/terraform/wrapper/main.tf
@@ -1,4 +1,11 @@
-# Employee App ci Terraform wrapper main.tf
 module "employee_app" {
-  source = "../../../modules/employee-app-ci"
+  source         = "../../../modules/employee-app-ci"
+  
+  ami_id         = var.ami_id
+  instance_type  = var.instance_type
+  subnet_id      = var.subnet_id
+  environment    = var.environment
+  desired_capacity = var.desired_capacity
+  min_size       = var.min_size
+  max_size       = var.max_size
 }
--- src/terraform/wrapper/variables.tf
+++ src/terraform/wrapper/variables.tf
@@ -3,3 +3,6 @@ variable "ami_id" { type = string }
 variable "instance_type" { type = string }
 variable "subnet_id" { type = string }
 variable "environment" { type = string }
+variable "desired_capacity" {}
+variable "min_size" {}
+variable "max_size" {}
